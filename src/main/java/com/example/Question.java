f1701d32a47ee59be9af5bdeda57c36c1bbc478d
PtNdhh <puspitadewi967@gmail.com>
2024-11-29 00:52:20 +0800

    buat project, pembuatan fitur dasar kuis

---
+++ src/main/java/com/example/Question.java
@@ -0,0 +1,20 @@
+package com.example;
+import java.util.List;
+
+public class Question {
+    private String question;
+    private List<String> options;
+    private String answer;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+}
